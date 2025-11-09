92cfe6c871ae382d1d6af2f21f967c1be402b749
Ben Lipton <ben.lipton@gmail.com>
2025-11-09 11:56:08 -0500

    Add back vpc network

--- prod/main.tf
+++ prod/main.tf
@@ -21,7 +21,6 @@ provider "google" {
   zone    = "us-central1-c"
 }
 
-# resource "google_compute_network" "vpc_network" {
-#   name = "terraform-network"
-# }
-
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
